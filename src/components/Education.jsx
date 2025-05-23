6f35b541730285926d6d00d461600d10ebd3267a
rain44556 <bristy44556@gmail.com>
2025-05-24 02:56:01 +0600

    edit utls

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,67 +1,8 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import eduExpData from "../utls/eduExpData"
 
 const Tabs = ["Publications", "Education", "Activities"];
-const data = {
-  education: [
-    {
-      title: "International Islamic University Chittagong",
-      subtitle: "Electrical and Electronic Engineering (EEE)",
-      location: "Kumira, Sitakundu, Chittagong",
-      date: "November 2018 - september 2023",
-      logo: "https://i.ibb.co.com/B24cPBNC/iiuc-logo.png",
-    },
-    {
-      title: "Hazera-Taju Degree College",
-      subtitle: "Higher Secondary School Certificate",
-      location: "Chandgoan, Chittagong",
-      date: "May 2016 - April 2018",
-      logo: "https://i.ibb.co.com/d34n806/HTUC-logo.jpg",
-    },
-    {
-      title: "SKQ Girls High School and College",
-      subtitle: "Secondary School Certificate",
-      location: "Mohara, Chittagong",
-      date: "January 2014 - February 2016",
-      logo: "https://i.ibb.co.com/Swdk3jKF/skq-girls-high-school-and-college-logo.png",
-    },
-  ],
-  publications: [
-    {
-      title:
-        "Hand Gesture Recognition Based Speed and Direction Control of DC Motor Using CNN",
-      subtitle: "Conference paper",
-      location: "Published in ICISET",
-      date: "October 2024",
-      logo: "https://i.ibb.co.com/rRXnSQY1/ieee.png",
-    },
-    {
-      title:
-        "Few Shot Learning for Medical Imaging: A Comparative Analysis of Methodologies and Formal Mathematical Framework",
-      subtitle: "Book Chapter",
-      location: "Published in Springer Nature",
-      date: "October 2023",
-      logo: "https://i.ibb.co.com/M5ktBkxN/images-1.jpg",
-    },
-    {
-      title:
-        "GSM Based System for Vehicle Gas Leakage Detection and Driver Condition Assessment",
-      subtitle: "Conference paper",
-      location: "Published in ICAECOT",
-      date: "October 2024",
-      logo: "https://i.ibb.co.com/rRXnSQY1/ieee.png",
-    },
-  ],
-  activities: [
-    {
-      title: "Open Source Contributor",
-      subtitle: "GitHub Projects",
-      location: "Online",
-      date: "2022 - Present",
-      logo: "https://i.ibb.co.com/nM1gDP9X/github-logo.png",
-    },
-  ],
-};
 
 const Education = () => {
   const [activeTab, setActiveTab] = useState("Education");
@@ -94,7 +35,7 @@ const Education = () => {
       <div className="relative w-full max-w-7xl mx-auto mt-16 md:p-0 p-5">
         <div className=" absolute left-1/2 transform -translate-x-1/2 w-1 bg-lime-100 h-full"></div>
 
-        {data[activeTab.toLowerCase()].map((item, index) => (
+        {eduExpData[activeTab.toLowerCase()].map((item, index) => (
           <motion.div
             key={index}
             initial={{ opacity: 0, y: 30 }}
---
+++ src/utls/eduExpData.js
@@ -0,0 +1,62 @@
+const eduExpData = {
+  education: [
+    {
+      title: "International Islamic University Chittagong",
+      subtitle: "Electrical and Electronic Engineering (EEE)",
+      location: "Kumira, Sitakundu, Chittagong",
+      date: "November 2018 - september 2023",
+      logo: "https://i.ibb.co.com/B24cPBNC/iiuc-logo.png",
+    },
+    {
+      title: "Hazera-Taju Degree College",
+      subtitle: "Higher Secondary School Certificate",
+      location: "Chandgoan, Chittagong",
+      date: "May 2016 - April 2018",
+      logo: "https://i.ibb.co.com/d34n806/HTUC-logo.jpg",
+    },
+    {
+      title: "SKQ Girls High School and College",
+      subtitle: "Secondary School Certificate",
+      location: "Mohara, Chittagong",
+      date: "January 2014 - February 2016",
+      logo: "https://i.ibb.co.com/Swdk3jKF/skq-girls-high-school-and-college-logo.png",
+    },
+  ],
+  publications: [
+    {
+      title:
+        "Hand Gesture Recognition Based Speed and Direction Control of DC Motor Using CNN",
+      subtitle: "Conference paper",
+      location: "Published in ICISET",
+      date: "October 2024",
+      logo: "https://i.ibb.co.com/rRXnSQY1/ieee.png",
+    },
+    {
+      title:
+        "Few Shot Learning for Medical Imaging: A Comparative Analysis of Methodologies and Formal Mathematical Framework",
+      subtitle: "Book Chapter",
+      location: "Published in Springer Nature",
+      date: "October 2023",
+      logo: "https://i.ibb.co.com/M5ktBkxN/images-1.jpg",
+    },
+    {
+      title:
+        "GSM Based System for Vehicle Gas Leakage Detection and Driver Condition Assessment",
+      subtitle: "Conference paper",
+      location: "Published in ICAECOT",
+      date: "October 2024",
+      logo: "https://i.ibb.co.com/rRXnSQY1/ieee.png",
+    },
+  ],
+  activities: [
+    {
+      title: "Open Source Contributor",
+      subtitle: "GitHub Projects",
+      location: "Online",
+      date: "2022 - Present",
+      logo: "https://i.ibb.co.com/nM1gDP9X/github-logo.png",
+    },
+  ],
+};
+
+export default eduExpData;
